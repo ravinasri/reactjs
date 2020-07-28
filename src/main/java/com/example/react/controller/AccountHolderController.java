007c24a12f4f76067f6b5f7bee97ef04595a46eb
ravina <t.ravinacbe@gmail.com>
2020-07-28 22:41:02 +0530

    My third Commit

--- src/main/java/com/example/react/controller/AccountHolderController.java
+++ src/main/java/com/example/react/controller/AccountHolderController.java
@@ -2,12 +2,16 @@ package com.example.react.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,6 +48,17 @@ public class AccountHolderController {
     }
   }
   
+
+//  @GetMapping("/accounts/{customer_id}")
+//  public ResponseEntity<AccountHolder> getCustomersById(@PathVariable("customer_id") String customer_id) {
+//    Optional<AccountHolder> Data = accountRepository.findById(customer_id);
+//
+//    if (Data.isPresent()) {
+//      return new ResponseEntity<>(Data.get(), HttpStatus.OK);
+//    } else {
+//      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+//  }
   
   @PostMapping("/accounts")
   public ResponseEntity<AccountHolder> create(@RequestBody AccountHolder accountholder) {
@@ -54,4 +69,42 @@ public class AccountHolderController {
       return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
     }
   }
+  
+//  @PutMapping("/accounts/{customer_id}")
+//  public ResponseEntity<AccountHolder> updateTutorial(@PathVariable("customer_id") String customer_id, @RequestBody AccountHolder accountHolder) {
+//    Optional<AccountHolder> updateData = accountRepository.findById(customer_id);
+//
+//    if (updateData.isPresent()) {
+//    	AccountHolder account = updateData.get();
+//    	account.setCustomer_name(accountHolder.getCustomer_name());
+//    	account.setCustomer_address(accountHolder.getCustomer_address());
+//    	account.setAccount_type(accountHolder.getAccount_type());
+//    	account.setAccount_number(accountHolder.getAccount_number());
+//    	account.setNomination(accountHolder.getNomination());
+//      
+//      return new ResponseEntity<>(accountRepository.save(account), HttpStatus.OK);
+//    } else {
+//      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+//  }
+  
+//  @DeleteMapping("/accounts/{customer_id}")
+//  public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("customer_id") String customer_id) {
+//    try {
+//    	accountRepository.deleteById(customer_id);
+//      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    } catch (Exception e) {
+//      return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+//    }
+//  }
+//
+//  @DeleteMapping("/accounts")
+//  public ResponseEntity<HttpStatus> deleteAllTutorials() {
+//    try {
+//      accountRepository.deleteAll();
+//      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    } catch (Exception e) {
+//      return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+//    }
+//  }
 }
